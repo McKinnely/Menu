36f1a5ca0ca6cd29a6b28c047c4aefe7bab15fee
McKinnely <McKinnely.bentley@gmail.com>
2016-11-04 14:51:34 -0500

    Sending Menus To Logs...

---
+++ app/src/main/java/com/example/mckinnely/menu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mckinnely.menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view)
+    {
+        // Find first menu item TextView and print the text to the logs
+        TextView firstItem = (TextView) findViewById(R.id.menu_item_1);
+        String menuItem1 = firstItem.getText().toString();
+        Log.v("MainActivity", menuItem1);
+
+
+        // Find second menu item TextView and print the text to the logs
+
+        // Find third menu item TextView and print the text to the logs
+
+    }
+}
